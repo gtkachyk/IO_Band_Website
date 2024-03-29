e07657b31ad797d99c4e3760f0ba513b49adae4a
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-03-29 14:14:31 -0600

    Made small styling changes to album pages

--- frontend/src/Components/ArtworkListElement.jsx
+++ frontend/src/Components/ArtworkListElement.jsx
@@ -2,7 +2,7 @@ function ArtworkListElement({paths}){
     return(
         <li>
             <a href={`${'../' + paths[0]}`} download>
-                <img src={`${'../' + paths[1]}`}></img>
+                <img className="art-list-element" src={`${'../' + paths[1]}`}></img>
             </a>
         </li>
     );
--- frontend/src/albums.js
+++ frontend/src/albums.js
@@ -34,7 +34,7 @@ export function generatePlaylistHTML(album, album_songs){
     var div_tags = ``;
     for(var i = 0; i < album_songs.length; i++){
         source_tags += `<source src=${"\"..\\" + album.path + '/audio/' + album_songs[i].audio_file_name + "\""} type="audio/wav" data-track-number=${"\"" + album_songs[i].track_number + "\""}/>`;
-        div_tags += `<div className=\"playlist-row-content-container\"><div className=\"play-list-row\" data-track-row=${"\"" + album_songs[i].track_number + "\""}><div className=\"small-toggle-btn\"><i className=\"small-play-btn\"><span className=\"screen-reader-text\">Small toggle button</span></i></div><div className=\"track-number\">${album_songs[i].track_number}.</div><div className=\"track-title\"><a className=\"playlist-track\" href=\"#\" data-play-track=${"\"" + album_songs[i].track_number + "\""} style=\"pointer-events: none\">${album_songs[i].name}</a></div></div></div>`;
+        div_tags += `<div className=\"playlist-row-content-container\"><div className=\"play-list-row\" data-track-row=${"\"" + album_songs[i].track_number + "\""}><div className=\"small-toggle-btn\"><i className=\"small-play-btn\"><span className=\"screen-reader-text\">Small toggle button</span></i></div><div className=\"track-number\"><pre className=\"track-number-inner\"> ${album_songs[i].track_number}. </pre></div><div className=\"track-title\"><a className=\"playlist-track\" href=\"#\" data-play-track=${"\"" + album_songs[i].track_number + "\""} style=\"pointer-events: none\">${album_songs[i].name}</a></div></div></div>`;
     }
     return [source_tags, div_tags];
 }
