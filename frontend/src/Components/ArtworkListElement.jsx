8ffe7b7c7dec3cf8ae0a369dc2f11f35faf0f790
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-03-29 23:01:43 -0600

    Changed hover property of artwork list elements

--- frontend/src/Components/ArtworkListElement.jsx
+++ frontend/src/Components/ArtworkListElement.jsx
@@ -1,8 +1,10 @@
 function ArtworkListElement({paths}){
     return(
         <li>
-            <a href={`${'../' + paths[0]}`} download>
-                <img className="art-list-element" src={`${'../' + paths[1]}`}></img>
+            <a className="art-list-download-link" href={`${'../' + paths[0]}`} download>
+                <div className="art-list-element-hover-div" style={{ backgroundImage: `url('${'../' + paths[1]}')` }}>
+                    <span className="art-list-caption">Download</span>
+                </div>
             </a>
         </li>
     );
