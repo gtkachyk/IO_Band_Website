48922aabee86701ca5453f6521da5ca704dba39c
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-04-02 11:30:35 -0600

    Put additional safety checks in scrollToBottom

--- frontend/src/js/root.js
+++ frontend/src/js/root.js
@@ -1,13 +1,28 @@
 export function scrollToBottom(){
+    var retryAttempts = 0;
+    const allowedAttempts = 5;
     var i = 0;
+    const allowedIterations = 5;
     let interval = window.setInterval(function() {
-        var elem = document.getElementById('guestbook-display-textarea');
-        elem.scrollTop = elem.scrollHeight;
-        i++;
-        if (i >= 10){
-            window.clearInterval(interval);
+        var textarea = document.getElementById('guestbook-display-textarea');
+        var setToBottom = false;
+        if (textarea != null){
+            textarea.scrollTop = textarea.scrollHeight;
+            setToBottom = true;
         }
-    }, 1000);
+        i++;
+        console.log("Forcing scrollbar down for " + (5 - i) + " more iterations");
+        if (i >= allowedIterations){
+            retryAttempts++;
+            if (setToBottom == true || retryAttempts >= allowedAttempts){ // No more attempts
+                window.clearInterval(interval);
+            }
+            else {
+                console.log("Running scrollToBottom() again, retryAttempts = " + retryAttempts + "/" + retryAttempts);
+                i = 0;
+            }
+        }
+    }, 100);
 };
 
 scrollToBottom();
