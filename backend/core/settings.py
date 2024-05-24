33f8ede8389a2018681606e62edcabb917187ae4
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-05-24 16:07:59 -0600

    Updated static files again

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
