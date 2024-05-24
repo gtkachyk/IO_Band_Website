963ade16d53d1d943fc2dedd2a5e239bfaa5836b
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-05-24 15:52:59 -0600

    Created static files for backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,6 +109,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
