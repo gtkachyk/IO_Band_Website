4e00ded071152c110939726ebb17f357616cf004
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-05-28 18:36:43 -0600

    Reset _redirects and changed path in album.jsx

--- frontend/src/routes/album.jsx
+++ frontend/src/routes/album.jsx
@@ -39,7 +39,7 @@ function Album() {
         if (!loading) {
             const script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = '/src/js/playlist.js';
+            script.src = '../js/playlist.js';
             script.async = true;
             document.body.appendChild(script);
 
