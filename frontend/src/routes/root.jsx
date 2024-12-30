e6c1fa9bbe2b66be81d9822ee0818a41a6653f58
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-12-29 20:23:02 -0700

    Deployment bug fix

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -50,7 +50,7 @@ function Root () {
     <>
       <NavBar></NavBar>
       <SocialMediaBar></SocialMediaBar>
-      <MainContainer styleSheet={home.main_container_styles}>
+      <MainContainer styleSheet={'root'}>
         <ContentUnit title={contentUnits[0].title} columns={contentUnits[0].columns}></ContentUnit>
         <ContentUnit title={contentUnits[1].title} columns={contentUnits[1].columns}></ContentUnit>
         <ContentUnit title={contentUnits[2].title} columns={contentUnits[2].columns}></ContentUnit>
