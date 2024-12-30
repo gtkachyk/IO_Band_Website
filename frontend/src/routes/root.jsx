a4942c3fdc262a417c5008817bfa53e7d1ddb0e6
gtkachyk <126801466+gtkachyk@users.noreply.github.com>
2024-12-29 18:57:35 -0700

    Deployment bug fix

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -33,7 +33,7 @@ function Root () {
     {
       title: 'Intentional Offence',
       columns: [
-        { header: "'II 0 II III' - Out Now!", content: <AlbumLink title={featured_album.display_name} image={home.featured_audio_media.image} link={home.featured_audio_media.link} styleSheet={`../styles/routes/root/overwrite/album_link.scss`}></AlbumLink>, id: 'featured-column-1' },
+        { header: "'II 0 II III' - Out Now!", content: <AlbumLink title={featured_album.display_name} image={home.featured_audio_media.image} link={home.featured_audio_media.link} styleSheet={`/styles/routes/root/overwrite/album_link.scss`}></AlbumLink>, id: 'featured-column-1' },
         { header: 'From the Network', content: <TikTokEmbed className="featured-tiktok" url={home.featured_social_media.tiktok} />, id: 'featured-column-2' },
       ],
     },
@@ -50,7 +50,7 @@ function Root () {
     <>
       <NavBar></NavBar>
       <SocialMediaBar></SocialMediaBar>
-      <MainContainer styleSheet={`../styles/routes/root/overwrite/content_unit.scss`}>
+      <MainContainer styleSheet={`/styles/routes/root/overwrite/content_unit.scss`}>
         <ContentUnit title={contentUnits[0].title} columns={contentUnits[0].columns}></ContentUnit>
         <ContentUnit title={contentUnits[1].title} columns={contentUnits[1].columns}></ContentUnit>
         <ContentUnit title={contentUnits[2].title} columns={contentUnits[2].columns}></ContentUnit>
